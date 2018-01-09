8f87ad5f3b11a8c4f5c6d4827ffcfdd80e1a0dc9
silvamateus <mateus.wtv@gmail.com>
2018-01-09 10:38:46 -0200

    create input component

---
+++ src/components/input.vue
@@ -0,0 +1,23 @@
+<template>
+  <input type="text" class="input-default">
+</template>
+
+<script>
+export default {
+  name: 'input-default',
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  input-default{
+    outline: none;
+    text-align: center left;
+    background-color: #fefefe;
+  }
+</style>
