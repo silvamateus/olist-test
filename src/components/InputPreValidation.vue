bc18b2b82d6f9debc93b080951c84a192176861a
silvamateus <mateus.wtv@gmail.com>
2018-01-09 16:21:53 -0200

    refactor: redo object of objects organization

---
+++ src/components/InputPreValidation.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <div v-for="value in inputData" :key="value.id">
+        <input-default v-bind:inputType="value"></input-default>
+    </div>
+  </div>
+</template>
+
+
+<script>
+import inputDefault from "./inputDefault";
+
+export default {
+  name: "InputPreValidation",
+  components: {
+    "input-default" : inputDefault
+  },
+
+  data() {
+    //Here data returns the Values we'll use to validate the password in the future
+    return {
+      inputData: {
+        nameInput: { typo: "text", label: "Nome Completo", id: "name" },
+        emailInput: { typo: "text", label: "E-mail", id: "e-mail" },
+        passInput: { typo: "password", label: "Senha", id: "password" }
+      }
+    }
+  }
+}
+</script>
