a16c753c1cb8cb96212f4f2d48b5d693a00b999d
silvamateus <mateus.wtv@gmail.com>
2018-01-10 12:00:58 -0200

    create logo component

---
+++ src/components/general-use/logo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <img src="\static\img\logotype.png" alt="olist">
+        <h2>Crie sua conta</h2>
+    </div>
+</template>
+
+
+<style scoped>
+
+    h2{
+        font-size: 22px;
+        padding-top: 0.8em;
+        padding-bottom: 1em;
+        margin: 0;
+        border: 0;
+    }
+</style>
