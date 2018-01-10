38943736118948c5f2a250114afe2d3bd9228963
silvamateus <mateus.wtv@gmail.com>
2018-01-10 11:59:58 -0200

    realocate archive to ./src/components/general-use

---
+++ src/components/general-use/inputDefault.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class='input-card'>
+    <label :for="inputType.id">{{inputType.label}}</label>
+    <input :type="inputType.typo" class='input-default' :id="inputType.id" >
+  </div>
+</template>
+
+<script>
+export default {
+  name: "inputDefault",
+  props: {
+    inputType: {
+      type: Object,
+      require: true
+    }
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.input-card{
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start; 
+  padding-bottom: 1em;
+}
+
+.input-default {
+  outline: none;
+  text-align: center left;
+  background-color: #fefefe;
+  border: 1px solid #e1e1e1;
+  width: 90%;
+  padding-top: 0.5em;
+  padding-bottom: 0.5em;
+  flex-grow: 1;
+}
+
+.input-card > label{
+  font-size: 12px;
+  padding-bottom: 4px;
+}
+</style>
