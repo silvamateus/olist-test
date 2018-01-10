55f2f96ddb0d015c7c5de46852ab84feb496d56b
silvamateus <mateus.wtv@gmail.com>
2018-01-10 12:01:34 -0200

    create password component

---
+++ src/components/InputPassValidation.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <div>
+          <passw-input :inputType="pass"></passw-input>
+      </div>
+  </div>
+</template>
+
+<script>
+
+    import inputDefault from "./general-use/inputDefault";
+    export default {
+
+        name: 'PassValidation',
+        components: {
+            'passw-input' : inputDefault
+        },
+
+        data(){
+            return{
+                pass: {typo: "password", label: "Senha", id: "password"},
+                passconf: {typo: "password", label: "Senha", id: "password-confirmation"}
+            }
+        }
+    
+    }
+</script>
+
+<style>
+
+</style>
